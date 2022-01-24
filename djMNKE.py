6d0bd20e3b319bb5def134f1f343267d761c69ff
leo <chathoo34@gmail.com>
2022-01-24 11:38:21 -0600

    Proof of Concept on how we're playing music is done

--- djMNKE.py
+++ djMNKE.py
@@ -27,9 +27,34 @@ print('DJ Monke Bot\n')
 
 bot = commands.Bot(command_prefix='mn!',intents=intents)
 
+tVoice = 828667775605669893
+
 @bot.event
 async def on_ready():
-	print('haha I dont do anything yet haha')
-	exit()
+	print('Ready!')
+
+@bot.command()
+async def play(ctx):
+	vc = await bot.get_channel(tVoice).connect()
+
+	vc.play(discord.FFmpegPCMAudio(executable="ffmpeg", source="dogbass.mp3"))
+
+	playStatus = vc.is_playing()
+	while playStatus:
+		await asyncio.sleep(1)
+		print('play')
+		playStatus = vc.is_playing()
+
+	await asyncio.sleep(2)
+
+	vc.play(discord.FFmpegPCMAudio(executable="ffmpeg", source="Think.flac"))
+
+	playStatus = vc.is_playing()
+	while playStatus:
+		await asyncio.sleep(1)
+		print('coom')
+		playStatus = vc.is_playing()
+
+	await ctx.guild.voice_client.disconnect()
 
 bot.run(TOKEN)
